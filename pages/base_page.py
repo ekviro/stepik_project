0522b180099eb2ad7839694b29e6693080394834
Ekaterina Ro <ekviro@gmail.com>
2023-05-06 23:40:09 +0300

    Тест добавления товара на 10 ссылках

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,7 +5,7 @@ import time
 
 
 class BasePage():
-    def __init__(self, browser, url, timeout=3):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,12 @@
 from .pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+link_start = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
+link = [f'{link_start}{i}' for i in range(10)]
+
+@pytest.mark.parametrize('link', link)
+def test_guest_can_add_product_to_basket(browser, link):
+    print(link)
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
