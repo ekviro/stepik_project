2a16230f78f42fc2739ec0561c28f4b9945a956b
Ekaterina Ro <ekviro@gmail.com>
2023-05-07 18:04:21 +0300

    Параметр для offer7 помечен как xfail

--- test_product_page.py
+++ test_product_page.py
@@ -4,9 +4,11 @@ import pytest
 link_start = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
 link = [f'{link_start}{i}' for i in range(10)]
 
+# помечаем ссылку offer7 как xfail
+link[7] = pytest.param(link[7], marks=pytest.mark.xfail)
+
 @pytest.mark.parametrize('link', link)
 def test_guest_can_add_product_to_basket(browser, link):
-    print(link)
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
