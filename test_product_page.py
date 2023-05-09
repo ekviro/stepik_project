cca55ed6ca25f1dd9ef8dddaf2bbfa8112dd97c9
Ekaterina Ro <ekviro@gmail.com>
2023-05-09 23:13:46 +0300

    Тесты для проверки логина со страницы продукта

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,6 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+
 import pytest
 
 link_offer_start = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
@@ -15,6 +17,22 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.add_product_to_basket()
 
+@pytest.mark.new
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+@pytest.mark.new
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
